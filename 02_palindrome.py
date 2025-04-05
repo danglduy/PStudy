5f3f72b14b32d6c5801f1b99f2ffcc81cf0a3f96
danglduy <38782010+danglduy@users.noreply.github.com>
2025-04-05 07:36:23 +0000

    02 - updates

--- 02_palindrome.py
+++ 02_palindrome.py
@@ -3,10 +3,28 @@ Palindrome Checker: Create a function that checks if a string is a palindrome (r
 """
 
 def check_palindrome(string):
-    return string == ''.join(reversed(string))
 
+    # reversed will return an Iterator
+    # return string == ''.join(reversed(string))
 
+    # using slicing syntax. Negative number at the end indicates we will go backward
+    # return string == string[::-1]
 
-print(check_palindrome("aaaaa"))
+    # two pointers
+
+    left = 0
+    right = len(string) - 1
+
+    while left < right:
+        if string[left] == string[right]:
+            left += 1
+            right -= 1
+            continue
+
+        return False
+
+    return True
+
+print(check_palindrome("assa"))
 
 
