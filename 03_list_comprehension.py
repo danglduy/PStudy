5c3907021b10636d1074b20615113269d14685be
danglduy <38782010+danglduy@users.noreply.github.com>
2025-04-05 09:55:11 +0000

    03

---
+++ 03_list_comprehension.py
@@ -0,0 +1,11 @@
+"""
+List Comprehension Practice: Write a function that takes a list of numbers and returns a new list containing only the even numbers.
+"""
+
+def list_even(numbers):
+    return [i for i in numbers if i % 2 == 0]
+
+print(list_even(range(10)))
+
+
+
